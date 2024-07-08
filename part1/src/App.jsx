fd2cf2afa0d8a09f1b10032ad4560923f91d0496
Curtinhas <carlos_curtinhas@sapo.pt>
2024-07-08 16:19:51 +0100

    Exercise 1.1 - done

---
+++ part1/src/App.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/prop-types */
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  // eslint-disable-next-line react/prop-types
+  return <h1>{props.course}</h1>;
+};
+
+const Parts = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Parts parts={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Parts parts={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Parts parts={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises:
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+export default App;
